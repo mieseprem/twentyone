e5991141d6a77a33e913dc226c2eef3bb48cf7ad
Clemens Nafe <clemens.nafe@otto.de>
2023-12-08 15:36:00 +0100

    move runner method up

--- src/main/java/de/kaasy/twentyone/TwentyOneApplication.java
+++ src/main/java/de/kaasy/twentyone/TwentyOneApplication.java
@@ -1,16 +1,59 @@
 package de.kaasy.twentyone;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RequiredArgsConstructor
+@Slf4j
 public class TwentyOneApplication {
     public static final long COMMON_WEB_TIMEOUT_IN_SECONDS = 10;
+    public static final ThreadFactory THREAD_FACTORY =
+            Thread.ofVirtual().name("virt-", 0L).factory();
+
+    final TwentyOneService twentyOneService;
 
     public static void main(String[] args) {
         var applicationContext = SpringApplication.run(TwentyOneApplication.class, args);
 
-        var twentyOneRunner = applicationContext.getBean(TwentyOneRunner.class);
-        twentyOneRunner.doYourWork();
+        var twentyOneRunner = applicationContext.getBean(TwentyOneApplication.class);
+        twentyOneRunner.doYourWork(20);
+    }
+
+    void doYourWork(final int numberOfItemsToProcess) {
+        log.info("Started");
+        // if you really want to limit the parallelism
+        try (var executorService = Executors.newFixedThreadPool(10, TwentyOneApplication.THREAD_FACTORY)) {
+            AtomicInteger batchProcess = new AtomicInteger();
+
+            // Simulate workload by generating a series of integers we'll later use as delay value
+            ThreadLocalRandom.current()
+                    .ints(
+                            numberOfItemsToProcess,
+                            1,
+                            Math.toIntExact(TwentyOneApplication.COMMON_WEB_TIMEOUT_IN_SECONDS) - 1)
+                    .forEach(intUsedForEverything -> {
+                        var batchNumber = batchProcess.incrementAndGet();
+
+                        executorService.submit(() -> {
+                            // vvvvvvvv "business logic" vvvvvvvv
+                            // #
+                            // # for every workload item data has to be collected
+                            var yesNoCatFact = twentyOneService.getData(batchNumber);
+                            // #
+                            // # afterward data should be written to somewhere
+                            twentyOneService.storeData(batchNumber, intUsedForEverything, yesNoCatFact);
+                            // #
+                            // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+                        });
+                    });
+        }
+        log.info("Done");
     }
 }
--- src/main/java/de/kaasy/twentyone/TwentyOneService.java
+++ src/main/java/de/kaasy/twentyone/TwentyOneService.java
@@ -10,9 +10,6 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.atomic.AtomicInteger;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -20,46 +17,16 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 @Slf4j
-public class TwentyOneRunner {
+public class TwentyOneService {
     private final CatFactsClient catFactsClient;
     private final HttpBinClient httpBinClient;
     private final YesNoClient yesNoClient;
 
-    private static final ThreadFactory THREAD_FACTORY =
-            Thread.ofVirtual().name("virt-", 0L).factory();
-
-    void doYourWork() {
-        log.info("Started");
-        // if you really want to limit the parallelism
-        try (var executorService = Executors.newFixedThreadPool(10, THREAD_FACTORY)) {
-            AtomicInteger batchProcess = new AtomicInteger();
-
-            // Simulate workload by generating a series of integers we'll later use as delay value
-            ThreadLocalRandom.current()
-                    .ints(20, 1, Math.toIntExact(TwentyOneApplication.COMMON_WEB_TIMEOUT_IN_SECONDS) - 1)
-                    .forEach(intUsedForEverything -> {
-                        var batchNumber = batchProcess.incrementAndGet();
-
-                        executorService.submit(() -> {
-                            // vvvvvvvv "business logic" vvvvvvvv
-                            // #
-                            // # for every workload item data has to be collected
-                            var yesNoCatFact = getData(batchNumber);
-                            // #
-                            // # afterward data should be written to somewhere
-                            storeData(batchNumber, intUsedForEverything, yesNoCatFact);
-                            // #
-                            // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-                        });
-                    });
-        }
-        log.info("Done");
-    }
-
-    private YesNoCatFact getData(final int batchNumber) {
+    YesNoCatFact getData(final int batchNumber) {
         log.info("Batch: {}, Call services", batchNumber);
 
-        try (ExecutorService executorService = Executors.newThreadPerTaskExecutor(THREAD_FACTORY)) {
+        try (ExecutorService executorService =
+                Executors.newThreadPerTaskExecutor(TwentyOneApplication.THREAD_FACTORY)) {
             // call services in parallel - StructuredConcurrency is not yet GA in Java 21 -> do it the legacy way
             Future<CatFactResponse> catFactsResponseFuture = executorService.submit(catFactsClient::fact);
             Future<YesNoResponse> yesNoResponseFuture = executorService.submit(yesNoClient::api);
@@ -82,7 +49,7 @@ public class TwentyOneRunner {
     /**
      * If you look at the code: nothing is stored here. This is just to simulate writing to a very slow disc.
      */
-    private void storeData(final int batchNumber, final int delay, final YesNoCatFact yesNoCatFact) {
+    void storeData(final int batchNumber, final int delay, final YesNoCatFact yesNoCatFact) {
         log.info("Batch: {}, Store result will last {}s", batchNumber, delay);
         httpBinClient.delay(delay);
         log.info(
